0a044bf5613256c0d6ffcdebd1af163d381fa31a
acupy <adam.czuprak@hotmail.com>
2017-06-03 19:26:06 -0400

    updated example

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ```python
 from wowair import WowAirQuery
 
+# Fetch available cities
+cities = WowAirQuery.get_cities()
+for c in sorted(cities, key=lambda city: cities[city]):
+    print cities[c], c
+
+# Fetch tickets for the next week from Montreal to Dublin
 wowair = WowAirQuery(origin='YUL', destination='DUB')
 for ticket in wowair.get_tickets():
     print ticket
--- example.py
+++ example.py
@@ -1,5 +1,11 @@
 from wowair import WowAirQuery
 
-wowair = WowAirQuery(origin='YUL')
+# Fetch available cities
+cities = WowAirQuery.get_cities()
+for c in sorted(cities, key=lambda city: cities[city]):
+    print cities[c], c
+
+# Fetch tickets for the next week from Montreal to Dublin
+wowair = WowAirQuery(origin='YUL', destination='DUB')
 for ticket in wowair.get_tickets():
     print ticket
