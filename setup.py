8e8621df571d26c1f84afcf7dc218ba953d1ce38
acupy <adam.czuprak@hotmail.com>
2017-06-03 17:58:10 -0400

    Added setup files for release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wowair',
+  packages = ['wowair'],
+  version = '0.1',
+  description = 'WowAir Python API',
+  author = 'Adam Czuprak',
+  author_email = 'adam.czuprak@hotmail.com',
+  url = 'https://github.com/acupy/wowair_api',
+  download_url = 'https://github.com/acupy/wowair_api/archive/0.1.tar.gz',
+  keywords = ['wowair', 'api'],
+  classifiers = [],
+)
