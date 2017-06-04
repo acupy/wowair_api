ac2a52afaa77f392fdb1703c2f8c4ba586fbf16f
acupy <adam.czuprak@hotmail.com>
2017-06-04 01:07:34 -0400

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'wowair',
   packages = ['wowair'],
-  version = '0.1',
+  version = '0.4',
   description = 'WowAir Python API',
   author = 'Adam Czuprak',
   author_email = 'adam.czuprak@hotmail.com',
   url = 'https://github.com/acupy/wowair_api',
-  download_url = 'https://github.com/acupy/wowair_api/archive/0.3.tar.gz',
+  download_url = 'https://github.com/acupy/wowair_api/archive/0.4.tar.gz',
+  install_requires=['requests'],
   keywords = ['wowair', 'api'],
   classifiers = [],
 )
